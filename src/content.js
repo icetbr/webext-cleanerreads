ee171af0e0e2a7c91ece1a94da3bec2419bc8c8a
icetbr <icetbr@yahoo.com.br>
2023-01-11 21:46:34 -0300

    fix sections: reading, want to read, published date, pages

--- src/content.js
+++ src/content.js
@@ -1,4 +1,22 @@
 import style from './style.css'
-import { addStyle } from '@icetbr/utils/web';
+import { addStyle, $, waitForEl } from '@icetbr/utils/web';
 
 addStyle(style);
+
+// 352 pages, Kindle Edition
+const pagesRegex = /(.*pages).*/;
+$('p[data-testid="pagesFormat"]').innerText = $('p[data-testid="pagesFormat"]').innerText.replace(pagesRegex, '$1');
+
+// Expected publication January 19, 2023
+const dateRegex = /.*((January|February|March|April|May|June|July|August|September|October|November|December).*)/;
+$('p[data-testid="publicationInfo"]').innerText = $('p[data-testid="publicationInfo"]').innerText.replace(dateRegex, '$1');
+
+// 263 people are currently reading
+waitForEl('div[data-testid="currentlyReadingSignal"]').then(el => {
+    el.innerText = el.innerText.replace(' people are currently reading', ' reading ·');
+})
+
+// 11.4k people want to read
+waitForEl('div[data-testid="toReadSignal"]').then(el => {
+    el.innerText = el.innerText.replace(' people', '');
+})
