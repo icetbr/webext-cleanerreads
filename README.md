5483fee5a5472241c0cadb599d96a7ca4e8b9acd
icetbr <icetbr@yahoo.com.br>
2023-02-21 15:21:15 -0300

    - fix: book contributors now shown on Bio section (this might need a fix if too many contributors) - fix: restoring the cover of the book when the review is from another edition - fix: profile menu opening behind the book cover - fix: book shelves not showing when trying to change the book shelf - fix: more editions arrows too far to the right - feat: topbar muted everywhere except the main feed

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Muting and moving some UI elements in favor of text. Nothing is removed, just re
 [Contributing](https://github.com/icetbr/my-projects/blob/main/CONTRIBUTING.md)\
 [License (MIT)](https://choosealicense.com/licenses/mit/)
 
-[1]: https://openuserjs.org/scripts/icetbr/CleanerReads_-_A_goodreads.com_Theme
+[1]: https://openuserjs.org/scripts/icetbr/CleanerReads_-_A_Goodreads_Theme
 [2]: https://addons.mozilla.org/en-US/firefox/addon/cleanerreads
 [3]: https://chrome.google.com/webstore/detail/cleanerreads/pfckicghmjgghmoeolegcbccjekhejji
 [4]: https://userstyles.world/style/8034/cleanerreads-a-goodreads-theme
--- src/content.js
+++ src/content.js
@@ -1,22 +1,39 @@
 import style from './style.css'
+import muteTopbar from './muteTopbar.css'
 import { addStyle, $, waitForEl } from '@icetbr/utils/web';
 
+var init = () => {
+    const isHomePage = location.href === 'https://www.goodreads.com/' || location.href.startsWith('https://www.goodreads.com/?');
+    if (!isHomePage) addStyle(muteTopbar);
+
+    const isBookPage = location.href.includes('/book/show/');
+    if (!isBookPage) return;
+
+    // 352 pages, Kindle Edition
+    const pagesRegex = /(.*pages).*/;
+    const $pagesFormat = $('p[data-testid="pagesFormat"]');
+
+    if ($pagesFormat) $pagesFormat.innerText = $pagesFormat.innerText.replace(pagesRegex, '$1');
+
+    // Expected publication January 19, 2023
+    const dateRegex = /.*((January|February|March|April|May|June|July|August|September|October|November|December).*)/;
+    const $publicationInfo = $('p[data-testid="publicationInfo"]');
+
+    if ($publicationInfo) $publicationInfo.innerText = $publicationInfo.innerText.replace(dateRegex, '$1');
+
+    // 263 people are currently reading
+    waitForEl('div[data-testid="currentlyReadingSignal"]').then(el => {
+        el.innerText = el.innerText.replace(' people are currently reading', ' reading ·');
+    })
+
+    // 11.4k people want to read
+    waitForEl('div[data-testid="toReadSignal"]').then(el => {
+        el.innerText = el.innerText.replace(' people', '');
+    })
+
+    // moves authors to the bio section
+    $('.AuthorPreview .ContributorLink').replaceWith($('.ContributorLinksList'));
+};
+
 addStyle(style);
-
-// 352 pages, Kindle Edition
-const pagesRegex = /(.*pages).*/;
-$('p[data-testid="pagesFormat"]').innerText = $('p[data-testid="pagesFormat"]').innerText.replace(pagesRegex, '$1');
-
-// Expected publication January 19, 2023
-const dateRegex = /.*((January|February|March|April|May|June|July|August|September|October|November|December).*)/;
-$('p[data-testid="publicationInfo"]').innerText = $('p[data-testid="publicationInfo"]').innerText.replace(dateRegex, '$1');
-
-// 263 people are currently reading
-waitForEl('div[data-testid="currentlyReadingSignal"]').then(el => {
-    el.innerText = el.innerText.replace(' people are currently reading', ' reading ·');
-})
-
-// 11.4k people want to read
-waitForEl('div[data-testid="toReadSignal"]').then(el => {
-    el.innerText = el.innerText.replace(' people', '');
-})
+init();
